bbbaae943b576cc01ba527c6dc303030df458965
RiaPermatasari1401 <riapermatsari0472@gmail.com>
2022-01-25 16:18:44 +0800

    Membuat route

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <div class="content-wrapper">
 
-      <?php include 'partials/content.php'; ?>
+      <?php include 'routes.php'; ?>
 
     </div>
 
