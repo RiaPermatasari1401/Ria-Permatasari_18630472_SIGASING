6b42abf95bf33fcefb14648b8ff4a6358259ed7b
RiaPermatasari1401 <riapermatsari0472@gmail.com>
2022-01-25 17:17:55 +0800

    Koneksi database dan datables

---
+++ partials/cssdatatables.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootsrap4.min.css">
+<link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootsrap4.min.css">
+<link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootsrap4.min.css">
