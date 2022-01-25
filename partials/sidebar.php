07a899ff8457adbd7c6c7b880bd343329e9f0188
RiaPermatasari1401 <riapermatsari0472@gmail.com>
2022-01-25 18:02:02 +0800

    Menampilkan halaman read lokasi

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -3,7 +3,6 @@
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
-
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
@@ -33,7 +32,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="?page=lokasiread" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Lokasi</p>
                             </a>
