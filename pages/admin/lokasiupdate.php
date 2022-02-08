c2bfa549f451eb962b03ee93fa9cda2de363d307
RiaPermatasari1401 <riapermatsari0472@gmail.com>
2022-02-08 16:36:51 +0800

    Membuat Perintah Update Data

--- pages/admin/lokasiupdate.php
+++ pages/admin/lokasiupdate.php
@@ -6,12 +6,46 @@ if (isset($_GET['id']) && $_GET['id'] <> '') {
     $id =  $_GET['id'];
     $findSql = "SELECT * FROM lokasi WHERE id = ?";
     $stmt = $db->prepare($findSql);
-    $stmt->bindParam(1, 'id');
+    $stmt->bindParam(1, $id);
     $stmt->execute();
     $row = $stmt->fetch();
     if (isset($row['id'])) {
+        if (isset($_POST['button_update'])) {
+
+            $database = new Database();
+            $db = $database->getConnection();
+
+            $validateSql = "SELECT * FROM lokasi WHERE nama_lokasi = ? AND id != ?";
+            $stmt = $db->prepare($validateSql);
+            $stmt->bindParam(1, $_POST['nama_lokasi']);
+            $stmt->bindParam(2, $_POST['id']);
+            $stmt->execute();
+            if ($stmt->rowCount() > 0) {
 
 ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    <h5><i class="icon fas fa-ban"></i> Gagal</h5>
+                    Nama lokasi sama sudah ada
+                </div>
+        <?php
+            } else {
+                $updateSql = "UPDATE lokasi SET nama_lokasi = ? WHERE id = ?";
+                $stmt = $db->prepare($updateSql);
+                $stmt->bindParam(1, $_POST['nama_lokasi']);
+                $stmt->bindParam(2, $_POST['id']);
+                if ($stmt->execute()) {
+                    $_SESSION['hasil'] = true;
+                    $_SESSION['pesan'] = "Berhasil ubah data";
+                } else {
+                    $_SESSION['hasil'] = true;
+                    $_SESSION['pesan'] = "Gagal ubah data";
+                }
+                echo '<meta http-equiv="refresh" content="0; url=?page=lokasiread">';
+            }
+        }
+        ?>
+
         <!-- disini koding untuk formnya -->
         <section class="content-header">
             <div class="container-fluid">
@@ -19,7 +53,6 @@ if (isset($_GET['id']) && $_GET['id'] <> '') {
                     <div class="col-sm-6">
                         <h1>Ubah Data Lokasi</h1>
                     </div>
-
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="?page=home">Home</a></li>
@@ -40,8 +73,8 @@ if (isset($_GET['id']) && $_GET['id'] <> '') {
                     <form method="post">
                         <div class="form-group">
                             <label for="nama_lokasi">Nama Lokasi</label>
-                            <input type="hidden" class="form-control" name="id" value="<?php echo $row['nama_lokasi'] ?>">
-                            <input type"text" class="form-control" name="nama_lokasi" value="<?php echo $row['nama_lokasi'] ?>">
+                            <input type="hidden" name="id" class="form-control" value="<?php echo $row['id'] ?>">
+                            <input type"text" name="nama_lokasi" class="form-control" value="<?php echo $row['nama_lokasi'] ?>">
                         </div>
                         <a href="?page=lokasiread" class="bth btn-danger btn-sm float-right ml-2">
                             <i class="fa fa-times"></i> Batal
@@ -53,13 +86,12 @@ if (isset($_GET['id']) && $_GET['id'] <> '') {
                 </div>
             </div>
         </section>
+
         <?php include_once "partials/scripts.php" ?>
 <?php
     } else {
-        echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+        echo '<meta http-equiv="refresh" content="0; url=?page=lokasiread">';
     }
 } else {
-    echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+    echo '<meta http-equiv="refresh" content="0; url=?page=lokasiread">';
 }
-
-?>
