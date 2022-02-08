f769403d492555621697324b44c63701650cb04c
RiaPermatasari1401 <riapermatsari0472@gmail.com>
2022-02-08 17:31:15 +0800

    Membuat Perintah Hapus Data Lokasi

--- routes.php
+++ routes.php
@@ -17,6 +17,10 @@ if(isset($_GET['page'])) {
         case 'lokasiupdate':
             file_exists('pages/admin/lokasiupdate.php') ? include 'pages/admin/lokasiupdate.php' : include "pages/404.php";
             break;
+            include "pages/404.php";
+        case 'lokasidelete':
+            file_exists('pages/admin/lokasidelete.php') ? include 'pages/admin/lokasidelete.php' : include "pages/404.php";
+            break;
         default:
             include "pages/404.php";
     }
