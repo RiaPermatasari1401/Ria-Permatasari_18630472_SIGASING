5432684878d4cb97eaf665e3b49af8d203611746
RiaPermatasari1401 <riapermatsari0472@gmail.com>
2022-02-16 11:07:03 +0800

    Membuat Halaman Tampil Data Karyawan

--- pages/admin/lokasidelete.php
+++ pages/admin/lokasidelete.php
@@ -1,19 +1,19 @@
 <?php
-if(isset($_GET['id']) && $_GET['id'] <> ''){
+if(isset($_GET['id']) && $_GET['id'] <> '') {
     $database = new Database();
     $db = $database->getConnection();
 
+    $id = $_GET['id'];
     $deleteSql = "DELETE FROM lokasi WHERE id = ?";
     $stmt = $db->prepare($deleteSql);    
-    $stmt->bindParam(1, $_GET['id']);
-    if($stmt->execute()){
+    $stmt->bindParam(1, $id);
+    if($stmt->execute()) {
         $_SESSION['hasil'] = true;
         $_SESSION['pesan'] = "Berhasil hapus data";
-    }else{
+    } else {
         $_SESSION['hasil'] = false;
         $_SESSION['pesan'] = "Gagal hapus data";
     }
-    echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";    
-}else{
-    echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";    
 }
+    echo '<meta http-equiv="refresh" content="0; url=?page=lokasiread">';    
+
--- routes.php
+++ routes.php
@@ -13,13 +13,14 @@ if(isset($_GET['page'])) {
         case 'lokasicreate':
             file_exists('pages/admin/lokasicreate.php') ? include 'pages/admin/lokasicreate.php' : include "pages/404.php";
             break;
-            include "pages/404.php";
         case 'lokasiupdate':
             file_exists('pages/admin/lokasiupdate.php') ? include 'pages/admin/lokasiupdate.php' : include "pages/404.php";
             break;
-            include "pages/404.php";
         case 'lokasidelete':
             file_exists('pages/admin/lokasidelete.php') ? include 'pages/admin/lokasidelete.php' : include "pages/404.php";
+            break;
+        case 'karyawanread':
+            file_exists('pages/admin/karyawanread.php') ? include 'pages/admin/karyawanread.php' : include "pages/404.php";
             break;
         default:
             include "pages/404.php";
